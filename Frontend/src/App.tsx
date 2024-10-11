8d0e0fdd35b18568a658934af5459c28707eb46e
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-10-12 03:11:20 +0530

    Created client

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [latestmessage, setLatestmessage] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8080');
+    newSocket.onopen = () => {
+      console.log('Connection established');
+      setSocket(newSocket);
+      newSocket.send('Hello Server!');
+    }
+    newSocket.onmessage = (message) => {
+      console.log('Message received:', message.data);
+      setLatestmessage(message.data);
+    }
+    
+    return () => newSocket.close();
+  }, [])
+
+
+  if(!socket){
+    return <div>
+      Connecting to socket server.....
+    </div>
+  }
 
   return (
     <>
-      
-       
+      <input onChange={(e)=>{
+        setMessage(e.target.value)
+      }}></input>
+      <button onClick={()=>{
+        socket.send(message);
+      }}>Send
+      </button>
+      {latestmessage} 
     </>
   )
 }
